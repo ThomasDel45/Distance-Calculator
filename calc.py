05c5b1bc1799a9d31934917694148ea1732253e4
Thomas Del Rosario <ThomasDel17@csu.fullerton.edu>
2021-11-29 21:21:37 -0800

    update file

--- calc.py
+++ calc.py
@@ -1,8 +1,6 @@
 import difflib
 import webbrowser
 import time
-
-
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
@@ -15,7 +13,7 @@ def home():
     return render_template("index.html")
 
 
-
+'''
 @app.route('/result',methods=['POST', 'GET'])
 def result():
     output = request.form.to_dict()
@@ -24,40 +22,19 @@ def result():
 
 
     return render_template('index.html', name = name)
-    
+'''    
 
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/distance',methods=['POST', 'GET'])
 def distance(w1, w2):
 
     w1 = input('Enter first word: ')
     w2 = input('Enter seond word: ')
 
-#matrix of edit distance
+
     r = len(w1) + 1
     c = len(w2) + 1
-    align = ''
 
-    edit_distance = [[
+    word_distance = [[
         0 for i in range(c)
     ]
         for i in range(r)
@@ -66,10 +43,10 @@ def distance(w1, w2):
     print('\n','Matrix:')
 
     for i in range(1, r):
-        edit_distance[i][0] = i
+        word_distance[i][0] = i
 
     for i in range(1, c):
-        edit_distance[0][i] = i
+        word_distance[0][i] = i
 
     for column in range(1, c):
         for row in range(1, r):
@@ -78,11 +55,23 @@ def distance(w1, w2):
             else:
                 incr = 1
 
-            edit_distance[row][column] = min(edit_distance[row-1][column] + 1, edit_distance[row][column-1] + 1, edit_distance[row-1][column-1] + incr)
+            word_distance[row][column] = min(word_distance[row-1][column] + 1, word_distance[row][column-1] + 1, word_distance[row-1][column-1] + incr)
     
     for display in range(r):
-        print(edit_distance[display])
+        print(word_distance[display])
 
-    return edit_distance[row][column]
+    return word_distance[row][column]
+
+
+    return render_template('index.html', w1 = w1, w2 = w2)
 
 print('Edit Distance: ',distance('word1', 'word2'))
+    
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
